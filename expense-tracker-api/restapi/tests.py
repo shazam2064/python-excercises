e12f63b67a67613e907b9a67b79549a0268c1b09
shazam <gabrielsalomon.980m@gmail.com>
2021-03-25 08:35:10 -0400

    Added more files

--- expense-tracker-api/restapi/tests.py
+++ expense-tracker-api/restapi/tests.py
@@ -1,3 +1,65 @@
+# from unittest import TestCase
 from django.test import TestCase
+from django.urls import reverse
+from restapi import models
+
 
 # Create your tests here.
+class TestModels(TestCase):
+    def test_expense(self):
+        expense = models.Expense.objects.create(
+            amount=249.99,
+            merchant="amazon",
+            description="anc headphones",
+            category="music",
+        )
+        inserted_expense = models.Expense.objects.get(pk=expense.id)
+
+        self.assertEqual(249.99, inserted_expense.amount)
+        self.assertEqual("amazon", inserted_expense.merchant)
+        self.assertEqual("anc headphones", inserted_expense.desciption)
+        self.assertEqual("music", inserted_expense.category)
+
+
+class TestViews(TestCase):
+    def test_expense_create(self):
+        payload = {
+            "amount": 50,
+            "merchant": "AT&T",
+            "description": "cell phone subscription",
+            "category": "utilities",
+        }
+
+        res = self.client.post(
+            reverse("restapi:expense-list-create"), payload, format="json"
+        )
+
+        self.assertEqual(201, res.status_code)
+
+        json_res = res.json
+
+        self.assertEqual(str(payload["amount"]), json_res["amount"])
+        self.assertEqual(payload["merchant"], json_res["merchant"])
+        self.assertEqual(payload["description"], json_res["description"])
+        self.assertEqual(payload["category"], json_res["category"])
+        self.assertEqual(json_res["id"], int)
+
+    def test_expense_list(self):
+        res = self.client.get(reverse("restapi:expense-list-create"), format="json")
+
+        self.assertEqual(200, res.status_code)
+
+        json_res = res.json()
+
+        self.assertIsInstance(json_res, list)
+
+        expenses = models.Expense.objects.all()
+        self.assertEqual(len(expenses), len(json_res))
+
+
+# def two_integers_sum(a, b):
+#     return a + b
+
+# class TestSum(TestCase):
+#     def test_sum(self):
+#         self.assertEqual(two_integers_sum(1, 2), 3)
