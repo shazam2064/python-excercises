541d7dcaa70781a2aa69979e40c0c989cf9e3ec4
shazam <gabrielsalomon.980m@gmail.com>
2021-03-28 09:25:35 -0400

    Added more files

--- graphql_social_media_api/graphql_api/tests.py
+++ graphql_social_media_api/graphql_api/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from graphene_django.utils.testing import GraphQLTestCase
+
+
+class GraphQLTest(GraphQLTestCase):
+    fixtures = ["users.json"]
+
+    def test_retrieve_by_id(self):
+        expected = {
+            "data": {
+                "user": {
+                    "id": "2",
+                    "name": "Dan Bilzerian",
+                    "followers": [{"name": "John Doe"}],
+                }
+            }
+        }
+        res = self.query(
+            """
+            {
+              user(id: 2) {
+                id
+                name
+                followers {
+                  name
+                }
+              }
+            }
+            """
+        )
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(expected, res.json())
+
 
 # Create your tests here.
---
+++ graphql_social_media_api/graphql_social_media_api/schema.py
@@ -0,0 +1,24 @@
+import graphene
+from graphene_django import DjangoObjectType
+
+from graphql_api import models
+
+
+class User(DjangoObjectType):
+    class Meta:
+        model = models.User
+
+
+class Query(graphene.ObjectType):
+    user = graphene.Field(User, id=graphene.Int())
+
+    def resolve_user(self, info, **kwargs):
+        id = kwargs.get("id")
+
+        if id is not None:
+            return models.User.objects.get(pk=id)
+
+        return None
+
+
+schema = graphene.Schema(query=Query)
